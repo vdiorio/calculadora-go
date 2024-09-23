4c6cffd2cbb089b4507a30a75c291b5a2b917514
Vitor Diorio <vitor.diorio@yahoo.com.br>
2024-09-23 19:39:13 -0300

    feat: Implementação básica da calculadora.
    - Adicionado implementação básica da calculadora com menu de opções para o usuário.

---
+++ calculadora.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var opcao int
+
+	for opcao != 5 {
+		fmt.Println("Calculadora")
+		fmt.Println("1. Soma")
+		fmt.Println("2. Subtração")
+		fmt.Println("3. Multiplicação")
+		fmt.Println("4. Divisão")
+		fmt.Println("5. Sair")
+		fmt.Scanln(&opcao)
+	}
+}
